402e9c264d95e5be673e412bf7fb87afe3393307
a7madsam <lolahmad987@gmail.com>
2022-04-09 00:33:30 +0300

    add validation for input & style it dynamicly

--- src/components/GoalForm/GoalFrom.js
+++ src/components/GoalForm/GoalFrom.js
@@ -3,20 +3,28 @@ import './GoalFrom.css';
 import Card from "../UI/Card";
 const GoalFrom = (props) => {
     const [goalInput, setGoalInput] = useState('');
+    const [isValid, setIsValid] = useState(true);
     const getDataHandler = (event) => {
+        if (event.target.value.trim().length > 0) {
+            setIsValid(true);
+        }
         setGoalInput(event.target.value);
     }
     const onSubmitHandler = (event) => {
         event.preventDefault();
+        if (goalInput.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
         props.onAddGoalClick({ id: new Date().getTime(), data: goalInput });
         setGoalInput('');
     }
     return (
         <Card className="goal-form">
             <form onSubmit={onSubmitHandler}>
-                <div className="goal-form__enter-area">
+                <div className={`goal-form__enter-area ${!isValid ? 'invalid' : ''}`}>
                     <label>Course Goal</label>
-                    <input type="text" value={goalInput} required onChange={getDataHandler} />
+                    <input type="text" value={goalInput} onChange={getDataHandler} />
                 </div>
                 <div className="goal-form__actions-area">
                     <button type="submit">Add Goal</button>
